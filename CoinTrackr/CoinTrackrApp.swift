018fdb2c558cdc5892e6166180b15d430171b53e
mahidimaduwage <mahidimaduwage@gmail.com>
2025-04-24 11:48:45 +0530

    Initial Commit

---
+++ CoinTrackr/CoinTrackrApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinTrackrApp.swift
+//  CoinTrackr
+//
+//  Created by Mahidi Maduwage on 2025-04-24.
+//
+
+import SwiftUI
+
+@main
+struct CoinTrackrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
