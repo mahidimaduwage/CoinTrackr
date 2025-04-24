b5bb530085a02d43f19b8d1cc468bf8b46c8ffa8
mahidimaduwage <mahidimaduwage@gmail.com>
2025-04-24 13:18:15 +0530

    initial project & custom color theme

---
+++ CoinTrackr/Extensions/Color.swift
@@ -0,0 +1,24 @@
+//
+//  Color.swift
+//  CoinTrackr
+//
+//  Created by Mahidi Maduwage on 2025-04-24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+    
+}
