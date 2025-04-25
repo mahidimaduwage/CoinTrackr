17cdc89f161f75df74ebcae10d156cb3c1c55282
mahidimaduwage <mahidimaduwage@gmail.com>
2025-04-25 13:33:41 +0530

    HomeView & navigation hear part done

---
+++ CoinTrackr/Core/Home/Views/HomeView.swift
@@ -0,0 +1,67 @@
+//
+//  HomeView.swift
+//  CoinTrackr
+//
+//  Created by Mahidi Maduwage on 2025-04-25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @State private var showPortfolio: Bool = false
+    
+    var body: some View {
+        ZStack {
+            // background layer
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            // content layer
+            VStack {
+                homeHeader
+
+                Spacer(minLength: 0)
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationView {
+        HomeView()
+            .navigationBarHidden(true)
+    }
+}
+
+extension HomeView {
+    
+    private var homeHeader: some View {
+        HStack {
+            CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                .transaction { $0.animation = nil }
+                .background(
+                    CircleButtonAnimationView(animate: $showPortfolio)
+                )
+            
+            Spacer()
+            
+            Text(showPortfolio ? "Portfolio" : "Live Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.accent)
+                .transaction { $0.animation = nil }
+            
+            Spacer()
+            
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        showPortfolio.toggle()
+                    }
+                }
+        }
+        .padding(.horizontal)
+    }
+}
